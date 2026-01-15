ffb7608c213b7f29f0f5de3943c86196a2ef28fe
David Calderon <david.calderon@stradata.com.co>
2026-01-15 14:09:11 -0500

    [Feat] Se ajustan los titulos

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 const app = express();
+const port = process.env.PORT || 3000 
 
 app.use(express.json());
 app.use(express.static('.')); // Sirve el index.html
